7565569ba45ba1c3ddd8a185a0b1a31f5d239a88
Busyvar <paul.lett@free.fr>
2020-09-26 23:50:31 +0200

    fix baudrate

--- over_the_rainbow.sh
+++ over_the_rainbow.sh
@@ -5,11 +5,13 @@ CMD_ARRET="0"
 CMD_ATTENTE="2"
 
 #Chemins
-SERIAL_PORT="/dev/ttyACM1"
+SERIAL_PORT="/dev/ttyACM0"
 AUDIO_FILE_PATH="/home/pi/sketchbook/over_the_skullbow/over_the_rain.mp3" 
-PAUSE_TIME=5 #in seconds
+PAUSE_TIME=5 #temps en seconde après la chanson
 
-function send { printf $1 > $SERIAL_PORT ;}
+function send {
+stty -F $SERIAL_PORT ospeed 9600 # fix baudrate
+printf $1 > $SERIAL_PORT ;}
 
 while [ 1 ];
 do
@@ -19,4 +21,3 @@ do
 	sleep $PAUSE_TIME 		# wait until restart
 done
 exit 0
-
