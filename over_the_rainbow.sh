783cc1ef49a10afe4dd69719e2ac69b67ad7aa4c
Busyvar <paul.lett@free.fr>
2020-09-27 17:46:11 +0200

    recup port serie generique

--- over_the_rainbow.sh
+++ over_the_rainbow.sh
@@ -5,7 +5,7 @@ CMD_ARRET="0"
 CMD_ATTENTE="2"
 
 #Chemins
-SERIAL_PORT="/dev/ttyACM0"
+SERIAL_PORT="/dev/ttyACM*"
 AUDIO_FILE_PATH="/home/pi/sketchbook/over_the_skullbow/over_the_rain.mp3" 
 PAUSE_TIME=5 #temps en seconde après la chanson
 
@@ -13,12 +13,15 @@ function send { printf $1 > $SERIAL_PORT ;}
 
 #stty -F $SERIAL_PORT ospeed 9600 # fix baudrate
 #chmod 777 $SERIAL_PORT
+
+while [ 1 ];
+do
+
 head -c 1 $SERIAL_PORT &
 send $CMD_ATTENTE
 sleep 5
 
-while [ 1 ];
-do
+
 	send $CMD_LANCEMENT  		# start skull sequence
 	omxplayer $AUDIO_FILE_PATH -o local	# play music
 	sleep $PAUSE_TIME 		# wait until restart
