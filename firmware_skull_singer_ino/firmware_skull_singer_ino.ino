a9f990c14ae67c1975ec88488b6304b844a3a882
PE LETT <paulemmanuel.lett@gmail.com>
2020-09-27 16:35:56 +0200

    Update firmware_skull_singer_ino.ino
    MODIF MOUVEMENT CRANE

--- firmware_skull_singer_ino/firmware_skull_singer_ino.ino
+++ firmware_skull_singer_ino/firmware_skull_singer_ino.ino
@@ -43,48 +43,96 @@ void lancement(){
   ferme();
   delay ( 200 ); // pre-buffering
   delay ( 235 );
-  move( 911,  ANGLE_1, "so"); // O
+  move( 711,  ANGLE_2, "so"); // M
+  delay ( 200 );
   move( 379,  ANGLE_3, "me"); // F
-  move( 1086, ANGLE_1, "whe"); //O
-  move( 578,  ANGLE_3, "re"); //F
-  move( 390,  ANGLE_1, "o"); //O
-  move( 312,  ANGLE_2, "ver"); //M
-  move( 575,  ANGLE_1, "the rai"); //O
-  move( 403,  ANGLE_2, "ain"); //M
-  move( 1162, ANGLE_1, "bow"); //O
-  move( 357,  ANGLE_3, "respiration"); //F
-  move( 753,  ANGLE_2, "blue"); //M
-  move( 864,  ANGLE_1, "bir"); //O
-  move( 659,  ANGLE_3, "ds"); //F
-  move( 1086, ANGLE_1, "fl"); //O
-  move( 1986, ANGLE_3, "ly"); //F
-  move( 1005, ANGLE_2, "birds"); //M
-  move( 794,  ANGLE_1, "fl"); //O
-  move( 665,  ANGLE_2, "ly"); //M
-  move( 426,  ANGLE_1, "o"); //O
-  move( 412,  ANGLE_3, "ver"); //F
-  move( 785,  ANGLE_2, "the rai"); //M
-  move( 493,  ANGLE_1, "ai"); //O
-  move( 469,  ANGLE_3, "n"); //F
-  move( 321,  ANGLE_1, "bo"); //O
-  move( 971,  ANGLE_3, "ow"); //F
-  move( 1580, ANGLE_1, "wh"); //O
-  move( 1516, ANGLE_3, "hy"); //F
-  move( 591,  ANGLE_2, "oh"); //M
+  delay ( 0 );
+  move( 400, ANGLE_2, "whe"); //M
+  delay ( 686 );
+  move( 500,  ANGLE_3, "re"); //F
+  delay ( 78 );
+  move( 100,  ANGLE_2, "o"); //M
+  delay ( 290 );
+  move( 50,  ANGLE_3, "V"); //F
+  delay ( 50 );
+  move( 150,  ANGLE_2, "ver"); //M
+  delay (62 );
+  move ( 50 , ANGLE_3, "t"); //F
+  delay (50);
+  move( 275,  ANGLE_2, "the rai"); //M
+  delay ( 303 );
+  move( 50,  ANGLE_3, "ain"); //F
+  delay ( 0 );
+  move( 362, ANGLE_1, "bow"); //O
+  delay ( 1000 );
+  move( 257,  ANGLE_3, "respiration"); //F
+  delay ( 100 );
+  move( 353,  ANGLE_2, "blue"); //M
+  delay ( 300 );
+  move( 600,  ANGLE_3, "bIR"); //O
+  delay ( 423 ); 
+  move( 50,  ANGLE_3, "ds"); //F
+  delay ( 0 );
+  move( 486, ANGLE_1, "fl"); //O
+  delay ( 900 );
+  move( 1086, ANGLE_3, "ly"); //F
+  delay ( 400 );
+  move( 205, ANGLE_2, "birds"); //M
+  delay ( 700 );
+  move( 100, ANGLE_3, "ds"); //F
+  delay ( 0 );
+  move( 359,  ANGLE_1, "flY"); //O
+  delay ( 800 );
+  move( 200,  ANGLE_3, "RESPI"); //F
+  delay ( 100 );
+  move( 226,  ANGLE_2, "ove"); //M
+  delay ( 200 );
+  move( 100,  ANGLE_3, "v"); //F
+  delay ( 0);
+  move( 112,  ANGLE_2, "ver"); //M
+  delay ( 100 );
+  move( 100,  ANGLE_3, "T"); //F
+  delay ( 0 );
+  move( 678,  ANGLE_2, "the rai"); //M
+  delay ( 500 ); 
+  move( 50,  ANGLE_3, "n"); //F 	
+  delay (0 );
+  move( 321,  ANGLE_1, "bo"); //M
+  delay ( 500 );
+  move( 71,  ANGLE_3, "ow"); //F 	
+  delay ( 0 ); 				
+  move( 1080, ANGLE_1, "wh"); //O
+  delay ( 1000 );
+  move( 816, ANGLE_3, "hy"); //F
+  delay ( 200 );
+  move( 491,  ANGLE_2, "oh"); //M
+  delay ( 100 );
   move( 780,  ANGLE_1, "wh"); //O
-  move( 747,  ANGLE_3, "hy"); //F
+  delay ( 400 );
+  move( 347,  ANGLE_3, "hy"); //F
+  delay ( 0);
   move( 852,  ANGLE_1, "can"); //O
-  move( 814,  ANGLE_3, "ant"); //F
+  delay ( 400 );
+  move( 314,  ANGLE_3, "ant"); //F
+  delay ( 100 );
   move( 1159, ANGLE_1, "t i"); //O
-  move( 1159, ANGLE_3, "iii"); //F
-  move( 947,  ANGLE_1, "why"); //O
-  move( 892,  ANGLE_2, "oh"); //M
+  delay ( 500 );
+  move( 559, ANGLE_3, "iii"); //F
+  delay ( 100 );
+  move( 647,  ANGLE_1, "why"); //O
+  delay ( 300 );
+  move( 692,  ANGLE_2, "oh"); //M
+  delay ( 200 );
   move( 580,  ANGLE_1, "wh"); //O
-  move( 981,  ANGLE_3, "hy"); //F
-  move( 2452, ANGLE_2, "can"); //M
+  delay ( 300 );
+  move( 581,  ANGLE_3, "hy"); //F
+  delay ( 100 );
+  move( 1452, ANGLE_2, "can"); //M
+  delay ( 1000 );
   move( 1120, ANGLE_1, "t iiiii"); //O
-  delay ( 2000 );
-  move( 2920, ANGLE_3, "iiiiiiii"); //F
+  delay ( 2850 ); 			
+  move(1920, ANGLE_3, "iiiiiiii"); //F
+  delay (1000 );
   //delay (192273 ) ; //ATTENTE 3 MIN
   DEBUG_FN_END
 }
